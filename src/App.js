33911ab2e25f86de9ec1416558fced34f27fa11d
trojanh <rajan.tiwari@kiprosh.com>
2019-03-14 16:39:51 +0530

    Simple TODO

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    term: '',
+    items: [],
+  }
+
+  handleInput = ({target: {value}}) =>{
+    this.setState({
+      term: value
+    })
+  }
+
+  addTodo = (event) =>{
+    const {items, term} =this.state
+    event.preventDefault();
+    this.setState({
+      term: '',
+      items: [...items, term]
+    })
+  }
+
+  deleteTodo = (index) =>{
+    console.log(index);
+
+    this.setState({
+      items: this.state.items.filter( (item, itemPosition) =>
+        index !== itemPosition
+      )
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+        <h1><u>Todos</u></h1>
+          {
+            this.state.items.map((item, index) =>
+                <li key={item+index}>
+                    {item}
+                  <button onClick={() => this.deleteTodo(index)}>X</button>
+                </li>
+
+              )
+          }
+          <form action="submit" onSubmit={this.addTodo}>
+            <input value={this.state.term} onChange={this.handleInput}/>
+            <button>Submit</button>
+          </form>
         </header>
       </div>
     );
